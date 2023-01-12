ad95cc2cf8176834dc3c8a6899019a88db9c3075
Chris Pfaff <contact@chrispfaff.dev>
2023-01-12 16:50:51 +0100

    Add tensors

--- executor1/executor.py
+++ executor1/executor.py
@@ -1,3 +1,6 @@
+import numpy as np
+import torch
+
 from jina import Executor, requests, DocumentArray
 
 
@@ -6,3 +9,8 @@ class MyExecutor(Executor):
     def foo(self, docs: DocumentArray, **kwargs):
         docs[0].text = 'hello, world!'
         docs[1].text = 'goodbye, world!'
+
+    @requests(on='/crunch-numbers')
+    def bar(self, docs: DocumentArray, **kwargs):
+        for doc in docs:
+            doc.tensor = torch.tensor(np.random.random([10, 2]))
